b2f37f207574ddfee9fb9ef890f8d18c4c459791
Pawan-ATR1004 <kumar.pawan@antiersolutions.com>
2024-09-24 13:21:30 +0530

    done

--- Facebook/social_media/src/main.jsx
+++ Facebook/social_media/src/main.jsx
@@ -3,11 +3,17 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import "bootstrap/dist/css/bootstrap.min.css";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router=createBrowserRouter(
+ [ {path:"/",element:<App/> } ]
+)
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <RouterProvider router={router}/>
+\  </StrictMode>,
 )
 
 
